8dc4aa169976205a93ab6af507042060e6311490
shohamatia <shohamatiaa@gmail.com>
2019-06-17 13:02:43 +0300

    corpus

--- src/processing/textStructure/Corpus.java
+++ src/processing/textStructure/Corpus.java
@@ -1,4 +1,5 @@
 package processing.textStructure;
+import com.sun.deploy.nativesandbox.NativeSandboxBroker;
 import processing.parsingRules.IparsingRule;
 import processing.parsingRules.STmovieParsingRule;
 import processing.parsingRules.STtvSeriesParsingRule;
@@ -7,6 +8,7 @@ import utils.MD5;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.io.Serializable;
 import java.util.*;
 
@@ -136,7 +138,7 @@ public class Corpus implements Iterable<Entry>, Serializable {
 	public void updateRAFs() {
 		for(Entry entry: entryList){
 		    for (Block block : entry){
-
+                new RandomAccessFile();
             }
         }
 	}
--- src/processing/textStructure/Entry.java
+++ src/processing/textStructure/Entry.java
@@ -1,19 +1,28 @@
 package processing.textStructure;
 
 import processing.parsingRules.IparsingRule;
+import processing.parsingRules.STmovieParsingRule;
+import processing.parsingRules.STtvSeriesParsingRule;
+import processing.parsingRules.SimpleParsingRule;
 
+import java.io.File;
+import java.io.RandomAccessFile;
 import java.io.Serializable;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * This class represents a single file within a Corpus
  */
 public class Entry implements Iterable<Block>, Serializable {
 	public static final long serialVersionUID = 1L;
+	transient RandomAccessFile inputFile;
+	List<Block> blocksList;
     
 
     public Entry(String filePath, IparsingRule parseRule) {
-    
+		File file = new File(filePath);
+		blocksList = parsingFactory(parseRule)
     }
 
 
@@ -25,6 +34,3 @@ public class Entry implements Iterable<Block>, Serializable {
     public Iterator<Block> iterator() {
     
     }
-
-	
-}
