b823e6efd8fa705b101c84fedff8e46001f54003
shohamatia <shohamatiaa@gmail.com>
2019-06-25 00:19:34 +0300

    got the idea of parsing rules major progress was made

--- src/processing/parsingRules/STmovieParsingRule.java
+++ src/processing/parsingRules/STmovieParsingRule.java
@@ -3,9 +3,14 @@ package processing.parsingRules;
 import processing.textStructure.Block;
 import processing.textStructure.WordResult;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.io.Serializable;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class STmovieParsingRule implements IparsingRule, Serializable {
@@ -15,11 +20,79 @@ public class STmovieParsingRule implements IparsingRule, Serializable {
 	}
 
 
+	private LinkedList<String[]> getScene(RandomAccessFile file){
+		String[] getSceneTitle = new String[4];
+		String sceneNumber = null;
+		String sceneName = null;
+		String sceneStart = null;
+		String sceneEnd = null;
+		final Pattern p = Pattern.compile(parsingRuleRegex.MOVIE_SCENE_EXTRACTOR);
+		final Matcher m;
+		//m = p.matcher(file.readLine());
+		//if (m.find()){
+			//sceneNumber = m.group("sceneNumber");
+			//sceneName = m.group("sceneName");
+			//int srt = m.start(3);
+			//int end = m.end(3);
+			//new Block()
+		//}
+		//getSceneTitle[0] = sceneNumber;
+		//getSceneTitle[1] = sceneName;
+		return null;
+	}
+
+	private String getTillFirstScene(RandomAccessFile file){
+		final Pattern sceneLine = Pattern.compile(parsingRuleRegex.SCENE_TITLE);
+		final Matcher thisLine = sceneLine.matcher("");
+		StringBuilder fileString = null;
+		try {
+			file.seek(0);
+			String nextLine = "";
+			String curLine;
+			while (thisLine.reset(nextLine).find()){
+				curLine = nextLine;
+				fileString.append(curLine);
+				nextLine = file.readLine();
+			}
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+		return String.valueOf(fileString);
+	}
+
+	private String[] getCredits(RandomAccessFile file){
+		String fileString = getTillFirstScene(file);
+		LinkedList<String> by = new LinkedList<>();
+		final Pattern getCreditsPattern = Pattern.compile(parsingRuleRegex.CREDITS);
+		assert fileString != null;
+		final Matcher matcherCredits = getCreditsPattern.matcher(fileString);
+		String credits = " ";
+		if (matcherCredits.find()){
+			credits = matcherCredits.group();
+		}
+		final Pattern p = Pattern.compile(parsingRuleRegex.CREDIT_SPLITTER);
+		final Matcher m = p.matcher(credits);
+		while (m.find()){
+			by.add(m.group("whatIsBy") + ": " + m.group("byWhom"));
+		}
+		return (String[]) by.toArray();
+	}
+
+	private String[] getSpeakers(String file){
+		LinkedList<String> getSpeaker = new LinkedList<>();
+		final Pattern p = Pattern.compile(parsingRuleRegex.MOVIE_SPEAKER);
+		final Matcher m = p.matcher(file);
+		while (m.find()){
+			getSpeaker.add(m.group());
+		}
+		return (String[]) getSpeaker.toArray();
+	}
+
 
 	@Override
 	public Block parseRawBlock(RandomAccessFile inputFile, long startPos, long endPos) {
 		return null;
-
 	}
 
 	@Override
---
+++ src/processing/parsingRules/parsingRuleRegex.java
@@ -0,0 +1,20 @@
+package processing.parsingRules;
+
+public class parsingRuleRegex {
+    static final String SCENE_TITLE = "(?<sceneNumber>\\d+)\\s+(?<sceneName>.*?)\\s+\\k<sceneNumber>";
+    static final String CREDITS = "^(\\s*(?<whatIsBy>\\w+) by:\\s*?(?:\\r\\n?|\\n)\\s+(?<byWhom>" +
+            "(?:[^\\r\\n]+)+))+";
+    static final String CREDIT_SPLITTER = "(\\s*(?<whatIsBy>\\w+) by:\\s*?(?:\\r\\n?|\\n)\\s+(?<byWhom>" +
+            "(?:[^\\r\\n]+)+))";
+    static final String SPEAKER = "?:^\\s+)(?<name>[A-Z]+)(?:$|(?:'S)?\\sVOICE)";
+    static final String MOVIE_SPEAKER = "^\\s{43}((?:\\S+\\s))+";
+    static final String MOVIE_SCENE_EXTRACTOR = "(?m)(?<sceneNumber>\\d+)\\s+(?<sceneName>.*?)" +
+            "\\s+\\k<sceneNumber>\\R(\\s{19}.*)+\\R";
+    static final String MOVIE_SCENE_HEADER = "^\\s+(?<sceneNum>\\d+)\\w?\\s+(?<sceneTitle>[^\\r\\n]+?)" +
+            "?\\s+\\k<SCENE_NUM>\\n";
+    static final String TV_HEADER = "(?m)\\A\\s+STAR TREK: THE NEXT GENERATION\\s+\"" +
+            "(?<episodeTitle>[^\\\"]+)\"\\s+([^\\r\\n])\" +\"*by\\s+(?<by_1>[^\\r\\n]+)\\s+(?:and\\s+" +
+            "(?<by2>[^\\r\\n]+))*\\";
+    static final String TV_SCENE = "(?<sceneNumber>\\d+)\\s+(?<sceneName>[^\\r\\n]+)\\R";
+    static final String CHARACTERS = "^\\t{5}(?<character>[^\\t\\r\\n]+)[\\r\\n]";
+}
