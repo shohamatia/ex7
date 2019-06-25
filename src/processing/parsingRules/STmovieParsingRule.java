5d6b02008ce4b8757d26225da7476c988f01c576
shohamatia <shohamatiaa@gmail.com>
2019-06-25 11:13:00 +0300

    movie parseFile

--- src/processing/parsingRules/STmovieParsingRule.java
+++ src/processing/parsingRules/STmovieParsingRule.java
@@ -3,12 +3,13 @@ package processing.parsingRules;
 import processing.textStructure.Block;
 import processing.textStructure.WordResult;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.io.Serializable;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -20,49 +21,61 @@ public class STmovieParsingRule implements IparsingRule, Serializable {
 	}
 
 
-	private LinkedList<String[]> getScene(RandomAccessFile file){
-		String[] getSceneTitle = new String[4];
-		String sceneNumber = null;
-		String sceneName = null;
-		String sceneStart = null;
-		String sceneEnd = null;
+	private LinkedList<Block> getScene(RandomAccessFile file){
+		LinkedList<Block> blocks = new LinkedList<>();
+		String sceneNumber;
+		String sceneName;
 		final Pattern p = Pattern.compile(parsingRuleRegex.MOVIE_SCENE_EXTRACTOR);
-		final Matcher m;
-		//m = p.matcher(file.readLine());
-		//if (m.find()){
-			//sceneNumber = m.group("sceneNumber");
-			//sceneName = m.group("sceneName");
-			//int srt = m.start(3);
-			//int end = m.end(3);
-			//new Block()
-		//}
-		//getSceneTitle[0] = sceneNumber;
-		//getSceneTitle[1] = sceneName;
-		return null;
+		final Matcher m = p.matcher(" ") ;
+		getSceneBlock(file, true);
+		String curBlock;
+		String nextBlock = getSceneBlock(file, false);
+		while (!Objects.requireNonNull(nextBlock).isEmpty()){
+			curBlock = nextBlock;
+			m.reset(curBlock);
+			if (m.find()){
+				sceneName = m.group("sceneName");
+				sceneNumber = m.group("sceneNumber");
+				Block block = new Block(file, m.start(3), m.end(3));
+				LinkedList<String> metadata = new LinkedList<>();
+				metadata.add("Appearing in scene " + sceneNumber + ", titled \"" + sceneName + "\"");
+				metadata.add(getSpeakers(m.group(3)));
+				block.setMetadata(metadata);
+				blocks.add(block);
+			}
+			nextBlock = getSceneBlock(file, false);
+		}
+		return blocks;
 	}
 
-	private String getTillFirstScene(RandomAccessFile file){
+
+
+	private String getSceneBlock(RandomAccessFile file, boolean metadata){
 		final Pattern sceneLine = Pattern.compile(parsingRuleRegex.SCENE_TITLE);
 		final Matcher thisLine = sceneLine.matcher("");
-		StringBuilder fileString = null;
+		StringBuilder fileString = new StringBuilder();
 		try {
-			file.seek(0);
+			if (metadata){
+				file.seek(0);
+			}
+			if (file.getFilePointer() == file.length() - 1){
+				return null;
+			}
 			String nextLine = "";
 			String curLine;
-			while (thisLine.reset(nextLine).find()){
+			while (!thisLine.reset(nextLine).find()){
 				curLine = nextLine;
 				fileString.append(curLine);
 				nextLine = file.readLine();
 			}
 		} catch (IOException e) {
-
 			e.printStackTrace();
 		}
 		return String.valueOf(fileString);
 	}
 
 	private String[] getCredits(RandomAccessFile file){
-		String fileString = getTillFirstScene(file);
+		String fileString = getSceneBlock(file, true);
 		LinkedList<String> by = new LinkedList<>();
 		final Pattern getCreditsPattern = Pattern.compile(parsingRuleRegex.CREDITS);
 		assert fileString != null;
@@ -73,34 +86,52 @@ public class STmovieParsingRule implements IparsingRule, Serializable {
 		}
 		final Pattern p = Pattern.compile(parsingRuleRegex.CREDIT_SPLITTER);
 		final Matcher m = p.matcher(credits);
-		while (m.find()){
-			by.add(m.group("whatIsBy") + ": " + m.group("byWhom"));
+		while (m.find()) {
+			by.add(m.group("whatIsBy") + " By: " + "\"" + m.group("byWhom") + "\"");
 		}
 		return (String[]) by.toArray();
 	}
 
-	private String[] getSpeakers(String file){
-		LinkedList<String> getSpeaker = new LinkedList<>();
+	private String getSpeakers(String file){
+		StringBuilder getSpeaker = new StringBuilder();
 		final Pattern p = Pattern.compile(parsingRuleRegex.MOVIE_SPEAKER);
+		assert file != null;
 		final Matcher m = p.matcher(file);
 		while (m.find()){
-			getSpeaker.add(m.group());
+			getSpeaker.append(m.group()).append(" ,");
 		}
-		return (String[]) getSpeaker.toArray();
+		String speakersString = getSpeaker.toString();
+		if (speakersString.substring(speakersString.length() - 2).equals(" ,")){
+			speakersString = speakersString.substring(0, speakersString.length() - 2);
+		}
+		return speakersString;
 	}
 
-
-	@Override
 	public Block parseRawBlock(RandomAccessFile inputFile, long startPos, long endPos) {
 		return null;
 	}
 
-	@Override
 	public List<Block> parseFile(RandomAccessFile inputFile) {
-		return null;
+		StringBuilder entryCredits = new StringBuilder();
+		String[] creditsList = getCredits(inputFile);
+		for (String credit : creditsList){
+			entryCredits.append(credit).append(", ");
+		}
+		String credits = entryCredits.toString();
+		if (credits.substring(credits.length() - 2).equals(", ")){
+			credits = credits.substring(0, credits.length() - 2);
+		}
+		LinkedList<Block> blocks = getScene(inputFile);
+		for (Block block : blocks){
+			LinkedList<String> metadata = new LinkedList<>();
+			List<String> curMetadata = block.getMetadata();
+			metadata.addAll(curMetadata);
+			metadata.add(credits);
+			block.setMetadata(metadata);
+		}
+		return blocks;
 	}
 
-	@Override
 	public void printResult(WordResult wordResult)  {
 	
 	}
